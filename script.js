d52250c08a1fbe165dd35001bf43e3f900fdc871
sophiekearse <sophiekearse@gmail.com>
2026-04-14 20:44:48 +0000

    added transitions between questions as well added some more questions. i also adjusted the css to make it more visually appealing, and made it so you can't start the game until you enter the name. you also cant go to the next question until youve selected an answer

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // add javascript here
+// radio buttons disabled to start
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+radioButtons.forEach(button => {
+    button.disabled = true;
+});
+
+//radio buttons enabled when start button is clicked, start button and name input disabled when start button is clicked
+document.getElementById("start").addEventListener('click', function(event) {
+    radioButtons.forEach(button => {
+        button.disabled = false;
+        document.getElementById("start").disabled = true;
+        document.getElementById("name").disabled = true;
+    });
+});
+
+//name
+// document.getElementById("name").
+
+// Handle question transitions
+document.querySelectorAll('.submit').forEach(button => {
+    button.addEventListener('click', function() {
+        const questionDiv = this.closest('.question');
+        const selected = questionDiv.querySelector('input[type="radio"]:checked');
+        if (!selected) {
+            alert('Please select an answer.');
+            return;
+        }
+        questionDiv.style.display = 'none';
+        const next = this.getAttribute('data-next');
+        const nextQuestion = document.getElementById(next);
+        if (nextQuestion) {
+            nextQuestion.style.display = 'block';
+        }
+    });
+});
+
