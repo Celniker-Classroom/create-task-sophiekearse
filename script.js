0e88d3683a9794abfe7d5d70ff8af1425759daaa
sophiekearse <sophiekearse@gmail.com>
2026-04-15 21:13:14 +0000

    added the possible recommended songs to my list, and added some more questions to my function. also started building to the function to determine what song you will be recommended

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // add javascript here
 // radio buttons disabled to start
+
 const radioButtons = document.querySelectorAll('input[type="radio"]');
 
 radioButtons.forEach(button => {
     button.disabled = true;
+
+document.getElementById("nextQuestion").disabled = true;
 });
 
 //radio buttons enabled when start button is clicked, start button and name input disabled when start button is clicked
@@ -12,6 +15,8 @@ document.getElementById("start").addEventListener('click', function(event) {
         button.disabled = false;
         document.getElementById("start").disabled = true;
         document.getElementById("name").disabled = true;
+        document.getElementById("nextQuestion").disabled = false;
+
     });
 });
 
@@ -33,6 +38,89 @@ document.querySelectorAll('.submit').forEach(button => {
         if (nextQuestion) {
             nextQuestion.style.display = 'block';
         }
+
+    
+
     });
 });
 
+//possible suggestions
+const recommendedSongs = [
+    {
+        score: 0,
+        //pop
+        songs: [
+            { title: "Choosin' Texas", artist: "Ella Langley" },
+            { title: "Man I Need", artist: "Olivia Dean" },
+            { title: "I Just Might", artist: "Bruno Mars"},
+            { title: "Ordinary", artist: "Alex Warren" },
+            { title: "Swim", artist: "BTS"},
+            { title: "So Easy (To Fall in Love)", artist: "Olivia Dean"},
+            { title: "Dracula", artist: "Tame Impala & JENNIE"},
+            { title: "DTMF", artist: "Bad Bunny"},
+            { title: "The Great Divide", artist: "Noah Kahan"},
+            { title: "Stateside", artist: "PinkPatheress with Zara Larsson"},
+        ]
+    },
+    {
+        score: 1,
+        //rock
+        songs: [
+            { title: "The Adults Are Talking", artist: "The Strokes" },
+            { title: "Dumb", artist: "Nirvana"},
+            { title: "Spectre", artist: "Radiohead"},
+            { title: "Dance Yrself Clean", artist: "LCD Soundsystem"},
+            { title: "Spiderwebs", artist: "No Doubt"},
+            { title: "Black", artist: "Pearl Jam"},
+            { title: "Let It Happen", artist: "Tame Impala"},
+            { title: "All the Small Things", artist: "Blink-182"},
+            { title: "On Melancholy Hill", artist: "Gorillaz"},
+            { title: "In the End", artist: "Linkin Park"},
+            { title: "Sister Europe", artist: "Foo Fighters"},
+        ],
+
+        score: 2,
+        //country
+        songs: [
+            { title: "Choosin' Texas", artist: "Ella Langley" },
+            { title: "I Got Better", artist: "Morgan Wallen"},
+            { title: "I Remeber Everything", artist: "Zach Bryan, Kacey Musgraves"},
+            { title: "Be By You", artist: "Luke Combs"},
+            { title: "I Had Some Help", artist: "Post Malone, Morgan Wallen"},
+            { title: "Be Her", artist: "Ella Langley"},
+            { title: "Tennessee Whiskey", artist: "Chris Stapleton"},
+            { title: "Plastic Cigarette", artist: "Zach Bryan"},
+            { title: "Bloodline", artist: "Alx Warren, Jelly Roll"},
+            { title: "Worst Way", artist: "Riley Green"},
+        ],
+
+        score: 3,
+        //hip hop
+        songs: [
+            { title: "Insecurities", artist: "Lil Baby"},
+            { title: "Minks in Miami", artist: "Rick Ross, French Montana, Max B"},
+            { title: "Like That", artist: "Future, Metro Boomin, Kendrick Lamar"},
+            { title: "Father", artist: "Kanye West, Travis Scott"},
+            { title: "forever be mine (feat Wiskid)", artist: "Gunna"},
+            { title: "Sum 2 Prove", artist: "Lil Baby"},
+            { title: "Lovin on Me", artist: "Jack Harlow"},
+            { title: "TOO FAST", artist: "Future"},
+            { title: "Two Six", artist: "J. Cole"},
+            { title: "MR RECOUP", artist: "21 Savage, Drake"},
+
+    
+            
+        ]
+        }
+
+]
+
+function showRecommendation(score) {
+    const song = recommendedSongs.find(s => s.score === score);
+    if (song) {
+        document.getElementById("songTitle").textContent = song.title;
+        document.getElementById("songArtist").textContent = `Artist: ${song.artist}`;
+        document.getElementById("songGenre").textContent = `Genre: ${song.genre}`;
+        document.getElementById("results").style.display = "block";
+    }
+}
