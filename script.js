0fd396ea2189201cca359c25a937f221d24b98a8
sophiekearse <sophiekearse@gmail.com>
2026-04-18 20:08:00 +0000

    final draft, changed a few lines for clarity and cleaned up code to make easier to read

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ function calculateScore(numQuestions) {
     }
     return total;
 }
-}
+
    
 var songs = [
     { title: "Dumb", artist: "Nirvana", genre: "Grunge" },
     { title: "On Melancholy Hill", artist: "Gorillaz", genre: "Synth Pop" },
-    { title: "Black", artist: "Pearl Jam", genre: "Rock" },
     { title: "Strawberry Fields Forever", artist: "The Beatles", genre: "Rock" },
     { title: "Layla", artist: "Derek and the Dominos", genre: "Rock" },
 ]
 
 document.getElementById("submit").addEventListener("click", function() {
-    let score = calculateScore();
+
+    let score = calculateScore(8);
     document.getElementById("score").style.display = "block";
     document.getElementById("score").textContent = "Your score is: " + score + "/8";
     let songIndex;
@@ -30,13 +30,12 @@ document.getElementById("submit").addEventListener("click", function() {
         songIndex = 1;
     } else if (score <= 6) {
         songIndex = 2;
-    } else if (score <= 8) {
+    } else 
         songIndex = 3;
-    } else {
-        songIndex = 4;
-    }
+    
 
-    document.getElementById("song").textContent = "Song recommendation: " +songs[songIndex].title + " -- " + songs[songIndex].artist + " (" + songs[songIndex].genre + ")";
+    document.getElementById("song").textContent = "Song recommendation: " +songs[songIndex].title + " -- " + songs[songIndex].artist + 
+    " (" + songs[songIndex].genre + ")";
 
     }
 );
